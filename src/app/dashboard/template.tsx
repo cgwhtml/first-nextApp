6daff2a18d0216920736ff7eb24bc49a75490ce6
cgwhtml <2598968853@qq.com>
2024-12-15 23:15:43 +0800

    feat: next应用

--- src/app/dashboard/template.tsx
+++ src/app/dashboard/template.tsx
@@ -7,8 +7,10 @@ export default function DashboardTemplate({ children }: { children: React.ReactN
     
     return (
         <div className="border-2 border-dashed border-black p-4 w-1/2 mx-auto">
-            <h2>Dashboard template {count}</h2>
-            <button className="bg-black text-white p-2 my-4 rounded-md" onClick={() => setCount(count + 1)}>Increment</button>
+            <h2 className="text-center text-white">Dashboard template {count}</h2>
+            <div className="flex justify-center">
+                <button className="bg-black text-white p-2 my-4 rounded-md" onClick={() => setCount(count + 1)}>Increment</button>
+            </div>
             {children}
         </div>
     );
